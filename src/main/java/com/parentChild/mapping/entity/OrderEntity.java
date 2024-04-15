4dda2d5de402f09d23ad887812e033e5bea19ac8
Amarjeet <jaiswaramarjeet19@gmai.com>
2024-04-15 15:08:08 +0530

    Changes in Application.properties file

--- src/main/java/com/parentChild/mapping/entity/OrderEntity.java
+++ src/main/java/com/parentChild/mapping/entity/OrderEntity.java
@@ -6,11 +6,15 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-/*@Getter
+@Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor*/
+@AllArgsConstructor
 @Entity
 public class OrderEntity {
 
